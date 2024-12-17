110b37d4d44b00af7450a75d9be485d0c48d6752
Uliana <125895500+UlianaKomissarova@users.noreply.github.com>
2024-12-17 17:00:09 +0300

    init commit

---
+++ src/main/java/dev/uliana/socks_accounting/SocksAccountingApplication.java
@@ -0,0 +1,13 @@
+package dev.uliana.socks_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksAccountingApplication.class, args);
+	}
+
+}
